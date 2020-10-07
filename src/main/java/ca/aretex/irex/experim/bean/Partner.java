36126ce83fee66da1f09f62cdd5ca724fe7ee68b
Get <dalbiaz2008@gmail.com>
2020-10-07 19:29:45 +0100

    override toString method in ther to work with save

--- src/main/java/ca/aretex/irex/experim/bean/Partner.java
+++ src/main/java/ca/aretex/irex/experim/bean/Partner.java
@@ -4,8 +4,13 @@ import lombok.Data;
 
 @Data
 public class Partner extends Client{
-    private String nom_entreprise;
-    private String nom_contact;
-    private String email;
-    private String telephone;
+    private String company_name;
+    private String lead_name;
+    private String email_id;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return String.format("{\"lead_name\":\"%s\",\"company_name\":\"%s\",\"email_id\":\"%s\",\"phone\":\"%s\"}", lead_name, company_name, email_id, phone);
+    }
 }
