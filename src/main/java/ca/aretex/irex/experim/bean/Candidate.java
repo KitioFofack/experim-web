e6bd16eec08dd9b47ae32ba3624b6c8625600efe
Get <dalbiaz2008@gmail.com>
2020-10-07 09:19:19 +0100

    add extension

--- src/main/java/ca/aretex/irex/experim/bean/Candidate.java
+++ src/main/java/ca/aretex/irex/experim/bean/Candidate.java
@@ -3,7 +3,7 @@ package ca.aretex.irex.experim.bean;
 import lombok.Data;
 
 @Data
-public class Candidate {
+public class Candidate extends Client{
     private String nom;
     private String prenom;
     private String telephone;
