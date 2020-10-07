6c9bec9ce788eac534c7b89e15c19cac4255dbd2
Get <dalbiaz2008@gmail.com>
2020-10-07 19:35:12 +0100

    set to work with any Client type

--- src/main/java/ca/aretex/irex/experim/service/ERPNextService.java
+++ src/main/java/ca/aretex/irex/experim/service/ERPNextService.java
@@ -4,15 +4,12 @@ import ca.aretex.irex.experim.bean.Candidate;
 import ca.aretex.irex.experim.bean.Client;
 import ca.aretex.irex.experim.bean.Partner;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Request;
-import org.apache.catalina.connector.Response;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Service
@@ -28,8 +25,25 @@ public class ERPNextService {
     @Value("${erpnextPassword}")
     private String erpnextPassword;
 
-    public HttpStatus save(Client client) {
-        logger.info("Saving {} in backend {}", client, erpnextServerURL );
+    public HttpStatus save(Client clt) {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
+            okhttp3.MediaType mediaType = MediaType.parse("application/json");
+
+            okhttp3.RequestBody body = RequestBody.create(mediaType, ""+clt);
+            okhttp3.Request request = new Request.Builder()
+                    .url("https://capetc-dev.irex.aretex.ca/api/resource/Lead")
+                    .method("POST", body)
+                    .addHeader("Authorization", "token 6b751d8f5c688cb:88ace929ef10d55")
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Cookie", "sid=Guest")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return HttpStatus.CREATED;
     }
 }
