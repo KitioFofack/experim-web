3a0b8a11a90dd15457bcc7a6b5f568842e7095c8
Get <dalbiaz2008@gmail.com>
2020-10-07 09:09:26 +0100

    now extends Client

--- src/main/java/ca/aretex/irex/experim/bean/Partner.java
+++ src/main/java/ca/aretex/irex/experim/bean/Partner.java
@@ -3,7 +3,7 @@ package ca.aretex.irex.experim.bean;
 import lombok.Data;
 
 @Data
-public class Partner {
+public class Partner extends Client{
     private String nom_entreprise;
     private String nom_contact;
     private String email;
