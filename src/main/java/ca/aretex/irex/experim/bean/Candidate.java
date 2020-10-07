3fa9af9281ded8e4e9d60b7f8b6acd94371aa99d
Get <dalbiaz2008@gmail.com>
2020-10-07 19:28:35 +0100

    adapt variable name to the Lead one  and override toString method in ther to work with save

--- src/main/java/ca/aretex/irex/experim/bean/Candidate.java
+++ src/main/java/ca/aretex/irex/experim/bean/Candidate.java
@@ -4,8 +4,13 @@ import lombok.Data;
 
 @Data
 public class Candidate extends Client{
-    private String nom;
-    private String prenom;
-    private String telephone;
-    private String courriel;
+    private String lead_name;
+    //private String prenom;
+    private String phone;
+    private String email_id;
+
+    @Override
+    public String toString() {
+        return String.format("{\"lead_name\":\"%s\",\"email_id\":\"%s\",\"phone\":\"%s\"}", lead_name, email_id, phone);
+    }
 }
