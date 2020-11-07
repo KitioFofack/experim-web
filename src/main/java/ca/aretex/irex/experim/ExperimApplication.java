2f815cfc83fdcd564b20a13d06bf18041ecbfbfe
lsonwa <dalbiaz2008@gmail.com>
2020-11-07 09:05:33 +0100

    exclude security autoconfiguration

--- src/main/java/ca/aretex/irex/experim/ExperimApplication.java
+++ src/main/java/ca/aretex/irex/experim/ExperimApplication.java
@@ -3,10 +3,11 @@ package ca.aretex.irex.experim;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ExperimApplication {
 
 	public static void main(String[] args) {
